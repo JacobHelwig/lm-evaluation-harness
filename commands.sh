30123bff8e8feb8ab6216fa00551e61e499b5c07
JacobHelwig <jacob.a.helwig@gmail.com>
2025-10-15 15:24:35 -0500

    debug config

--- commands.sh
+++ commands.sh
@@ -2,12 +2,13 @@ MODEL_NAME=Qwen/Qwen2.5-0.5B-Instruct
 GPUS_PER_MODEL=1
 MODEL_REPLICAS=1
 TASK=minerva_math
-MAX_MODEL_LEN=8192
+MAX_MODEL_LEN=2048
 OUTPUT_PATH=./outputs
-GPU=7
+GPU=0
+GPU_MEMORY_UTILIZATION=0.2
 
 CMD="CUDA_VISIBLE_DEVICES=${GPU} python -m lm_eval --model vllm \
-    --model_args pretrained=${MODEL_NAME},tensor_parallel_size=${GPUS_PER_MODEL},dtype=auto,gpu_memory_utilization=0.8,data_parallel_size=${MODEL_REPLICAS},max_model_len=${MAX_MODEL_LEN} \
+    --model_args pretrained=${MODEL_NAME},tensor_parallel_size=${GPUS_PER_MODEL},dtype=auto,gpu_memory_utilization=${GPU_MEMORY_UTILIZATION},data_parallel_size=${MODEL_REPLICAS},max_model_len=${MAX_MODEL_LEN} \
     --batch_size auto \
     --tasks ${TASK} \
     --output_path ${OUTPUT_PATH} --log_samples --predict_only \
